84240732398c356268b9316042fe948b0d08c758
yzs020220 <754052428@qq.com>
2023-02-16 23:50:08 +0800

    Site updated: 2023-02-16 23:50:08

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -36,8 +36,9 @@ if (screen.width >= 768) {
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
-			cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/",
+			//cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/",
 			//cdnPath: "https://npm.elemecdn.com/yzs-live2d_src@latest/",
+			cdnPath: "https://cdn.jsdelivr.net/npm/yzs-live2d_src@latest/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
